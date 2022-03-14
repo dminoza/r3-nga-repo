6d6035d3dcd3d2293913c3e3d637a2f173148a8f
Dario Cruz Minoza <dminozadev@gmail.com>
2022-03-14 18:54:32 +0800

    added list, create, update and destroy api view

--- project-nako/project/posts/views.py
+++ project-nako/project/posts/views.py
@@ -3,20 +3,33 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
 
-class IndexView(generic.ListView):
-    template_name = 'post/index.html'
-    context_object_name = 'post_list'
+# class IndexView(generic.ListView):
+#     template_name = 'post/index.html'
+#     context_object_name = 'post_list'
 
-    def get_queryset(self):
-        return Post.objects.order_by('-created_at')[:4]
+#     def get_queryset(self):
+#         return Post.objects.order_by('-created_at')[:4]
 
 
-class DetailView(generic.DetailView):
-    model = Post
-    template_name = 'post/detail.html'
+# class DetailView(generic.DetailView):
+#     model = Post
+#     template_name = 'post/detail.html'
+
+
+class PostListCreateAPIView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
 
